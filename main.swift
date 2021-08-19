9dc2a500d77f30f2aa447e885832db24dfbd49a7
Kasperovi4 <kasperu@mail.ru>
2021-08-19 19:38:22 +0300

    homework5

--- main.swift
+++ main.swift
@@ -1,151 +1,142 @@
 //
 //  main.swift
-//  4Kasperovich
+//  5Kasperovich
 //
-//  Created by Дмитрий Касперович on 15.08.2021.
+//  Created by Дмитрий Касперович on 19.08.2021.
 //
 
 import Foundation
 
 
-enum Engine: String {
- case On = "Включен"
- case Off = "Выключен"
-}
-enum Window: String {
- case Open = "Открыты окна"
- case Close = "Закрыты окна "
-}
-enum Trunk: String {
-     case Yes = "Гружоный"
-     case Not = "Пустой"
+
+protocol Car {
+
+    var statWindow: StatWindow {get set}
+    var marka: String {get}
+    var statEngine: StatEngine {get set}
+    
+    func  statwindow()
+    func  statengine()
 }
 
+enum Privod: String {
+    case full = "Полный привод"
+    case notFull = "Не полный привод"
+}
 
- class Car {
-    
-     var description: String {
-         return """
-            Марка: \(marka)
-            Год выпуска: \(yearOfRelease)
-            Окна: \(window.rawValue)
-            Двигатель: \(engine.rawValue)
-            Гружоный: \(trunk.rawValue)
-            Привод: \(privod)
-            """
-     }
-     var marka: String
-     var yearOfRelease: Int
-     var engine: Engine
-     var window: Window
-     var trunk: Trunk
-     var privod: Int
-    
-    init (marka: String, yearOfRelease: Int, engine: Engine, window: Window, trunk: Trunk, privod: Int) {
-        self.marka = marka
-        self.yearOfRelease = yearOfRelease
-        self.engine = engine
-        self.window = window
-        self.trunk = trunk
-        self.privod = privod
-    }
-    func privodCar () {
-        print("\(privod) - Ведущих колес")
-    }
-    func trailerCar() {
-      print("Прицеп")
-    }
-    
-    
- }
+enum StatWindow: String {
+    case Open = "Открыты"
+    case Close = "Закрыты"}
 
+enum StatEngine: String {
+    case On = "Включен"
+    case Off =  "Выключен"}
+
+extension Car {
+   
+    
+    
+    func statwindow() {
+      if statWindow == .Open{
+           print ("\(marka) Окна открыты")
+    }
+        else if statWindow == .Close {
+           print("\(marka) Окна закрыты")
+        }
+    }
+    func statengine() {
+        if statEngine == .On{
+            print ("\(marka) Двигатель запущен")
+        }
+        else if statEngine == .Off {
+            print("\(marka) Двигатель заглушен")
+        }
+    }
+}
 
 class SportCar: Car, CustomStringConvertible {
     
-    
-    enum CarHatchState: String {
-        case Yes = "Есть люк"
-        case Not = "Нету люк "
-    
+    var description: String {
+        return """
+             Марка: \(marka)
+             Статус Окна: \(statWindow.rawValue)
+             Статус Двигателя: \(statEngine.rawValue)
+             Какой привод: \(privod.rawValue)
+             """
     }
     
-    var hatchState: CarHatchState
-    override var description: String {
-             return """
-                Марка: \(marka)
-                Год выпуска: \(yearOfRelease)
-                Окна: \(window.rawValue)
-                Двигатель: \(engine.rawValue)
-                Гружоный: \(trunk.rawValue)
-                Люк: \(hatchState.rawValue)
-                Привод: \(privod)
-                """
-        }
-    init(marka: String, yearOfRelease: Int, engine: Engine, window: Window, hatchState: CarHatchState, trunk: Trunk, privod: Int ) {
-        self.hatchState = hatchState
-        super.init(marka: marka, yearOfRelease: yearOfRelease, engine: engine, window: window, trunk: trunk, privod: privod)
+    var statWindow: StatWindow
+    var marka: String
+    var statEngine: StatEngine
+    var privod : Privod
+    
+    init(statWindow: StatWindow, marka: String, statEngine: StatEngine, privod : Privod){
+        self.privod = privod
+        self.marka = marka
+        self.statEngine = statEngine
+        self.statWindow = statWindow
     }
-    override func privodCar() {
-        if privod == 4{
-            print ("\(marka) Машина спортивная")
-            }
-        else if privod == 2 {
-            print("\(marka) Машина не спортивная")
-        }
+    func privodCar(privod: Privod) {
+        
+        self.privod = privod
+        print("У \(marka) теперь \(privod.rawValue)")
+        
+
     }
 }
-    
-    
-class TrankCar: Car, CustomStringConvertible {
-   
-    enum Trailerstatus: String {
-        case Yes = "Есть прицеп"
-        case Not = "Нету прицепа"
-    }
-    override var description: String {
-             return """
-                Марка: \(marka)
-                Год выпуска: \(yearOfRelease)
-                Окна: \(window.rawValue)
-                Двигатель: \(engine.rawValue)
-                Гружоный: \(trunk.rawValue)
-                Прицеп: \(trailer.rawValue)
-                Привод: \(privod)
-                """
-        }
-    var trailer: Trailerstatus
-    init(marka: String, yearOfRelease: Int, engine: Engine, window: Window, trailer: Trailerstatus, trunk: Trunk, privod: Int) {
-        self.trailer = trailer
-       
-        super.init(marka: marka, yearOfRelease: yearOfRelease, engine: engine, window: window, trunk: trunk, privod: privod)
+
+class trackCar: Car, CustomStringConvertible {
+    var description: String {
+        return """
+                 Марка: \(marka)
+                 Статус Окна: \(statWindow.rawValue)
+                 Статус Двигателя: \(statEngine.rawValue)
+                 Наличие Цистерны: \(statusCicterna.rawValue)
+                 """
     }
     
-    override func trailerCar() {
-        if trailer == .Yes {
-        print ("\(marka) Грузовая машина")
-            }
-        else if trailer == .Not {
-            print("\(marka) Не грузовая машина")
-        }
+    var statWindow: StatWindow
+    var marka: String
+    var statEngine: StatEngine
+    var statusCicterna : StatusCicterna
+    
+    init(statWindow: StatWindow, marka: String, statEngine: StatEngine, statusCicterna : StatusCicterna){
+        self.statusCicterna = statusCicterna
+        self.marka = marka
+        self.statEngine = statEngine
+        self.statWindow = statWindow
     }
- 
+    enum StatusCicterna: String {
+        case Yes = "Цистерна прицеплена"
+        case Not = "Цистерна отцеплена"}
+    
+    func trackCar(statusCicterna: StatusCicterna) {
+        
+        self.statusCicterna = statusCicterna
+        print("\(statusCicterna.rawValue) у машины \(marka)" )
+        
+    }
 }
 
-var sportCar1 = SportCar(marka: "Honda", yearOfRelease: 2020, engine: .On, window: .Open, hatchState: .Yes, trunk: .Not, privod: 2)
-var sportCar2 = SportCar(marka: "BMW", yearOfRelease: 2021, engine: .Off, window: .Close, hatchState: .Not, trunk: .Not, privod: 4)
-
-var trankCar1 = TrankCar(marka: "Volvo", yearOfRelease: 2000, engine: .On, window: .Open, trailer: .Yes, trunk: .Yes, privod: 2)
-var trankCar2 = TrankCar(marka: "Ivtko", yearOfRelease: 2005, engine: .Off, window: .Close, trailer: .Yes, trunk: .Yes, privod: 4)
-
-sportCar1.privodCar()
-sportCar2.privodCar()
-trankCar2.trailerCar()
-trankCar1.trailerCar()
+var sportCar1 = SportCar(statWindow: .Open, marka: "BMW", statEngine: .On, privod: .full)
+var sportCar2 = SportCar(statWindow: .Close, marka: "HONDA", statEngine: .Off, privod: .notFull)
 
 
 
+var trackCar1 = trackCar(statWindow: .Open, marka: "VOLVO", statEngine: .On, statusCicterna: .Yes)
+var trackCar2 = trackCar(statWindow: .Close, marka: "IVEKO", statEngine: .Off, statusCicterna: .Not)
+
+//print(sportCar1)
+//print(sportCar2)
+//print(trackCar1)
+//print(trackCar2)
+
+
+sportCar1.privodCar(privod: .notFull)
+sportCar2.statEngine = .On
+trackCar1.trackCar(statusCicterna: .Not)
+trackCar2.statWindow = .Open
 print(sportCar1)
 print(sportCar2)
-print(trankCar1)
-print(trankCar2)
-
+print(trackCar1)
+print(trackCar2)
